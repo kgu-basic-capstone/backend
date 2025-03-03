1eb6d1b050cb3b21c2dcf0c6379457dfafa84902
배영빈 <yeongbinbae01@gmail.com>
2025-03-03 13:34:32 +0000

    feat: 이메일 기반 회원가입 여부 확인 기능 추가

--- service/src/main/java/uk/jinhy/server/service/test/example/src/member/domain/MemberRepository.java
+++ service/src/main/java/uk/jinhy/server/service/test/example/src/member/domain/MemberRepository.java
@@ -1,6 +1,7 @@
-package uk.jinhy.server.service.test.example.src.member;
+package uk.jinhy.server.service.test.example.src.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
 }
