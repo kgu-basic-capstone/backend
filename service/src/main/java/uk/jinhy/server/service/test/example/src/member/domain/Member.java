9280ffde4206fb45246267898b50ea6228faad59
배영빈 <yeongbinbae01@gmail.com>
2025-03-03 13:41:33 +0000

    feat: 회원 엔티티 @Getter, @Builder 추가

--- service/src/main/java/uk/jinhy/server/service/test/example/src/member/domain/Member.java
+++ service/src/main/java/uk/jinhy/server/service/test/example/src/member/domain/Member.java
@@ -1,10 +1,13 @@
-package uk.jinhy.server.service.test.example.src.member;
+package uk.jinhy.server.service.test.example.src.member.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member {
@@ -23,4 +26,11 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private MembershipLevel membershipLevel;
 
+    @Builder
+    private Member(String name, int age, String email, MembershipLevel membershipLevel) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.membershipLevel = membershipLevel;
+    }
 }
