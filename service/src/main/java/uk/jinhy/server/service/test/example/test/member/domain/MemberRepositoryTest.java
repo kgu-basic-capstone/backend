8a26d7dbc9249280984e95fb60284a7d742f6af1
배영빈 <yeongbinbae01@gmail.com>
2025-03-03 13:41:47 +0000

    test: 이메일 기반 회원가입 여부 확인 테스트 추가

---
+++ service/src/main/java/uk/jinhy/server/service/test/example/test/member/domain/MemberRepositoryTest.java
@@ -0,0 +1,58 @@
+package uk.jinhy.server.service.test.example.test.member.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import uk.jinhy.server.service.test.example.src.member.domain.Member;
+import uk.jinhy.server.service.test.example.src.member.domain.MemberRepository;
+
+import static org.assertj.core.api.Assertions.*;
+import static uk.jinhy.server.service.test.example.src.member.domain.MembershipLevel.BASIC;
+
+@Component
+@Transactional
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @DisplayName("이미 가입된 이메일이면 true를 반환한다.")
+    @Test
+    void alreadyExists() {
+        //given
+        String email = "test@test.com";
+
+        Member testMember = Member.builder()
+                .name("testMember")
+                .age(20)
+                .email(email)
+                .membershipLevel(BASIC)
+                .build();
+
+        memberRepository.save(testMember);
+
+        //when
+        boolean result = memberRepository.existsByEmail(email);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("가입되지 않은 이메일이라면 false를 반환한다.")
+    @Test
+    void notExists() {
+        //given
+        String email = "test@test.com";
+
+        //when
+        boolean result = memberRepository.existsByEmail(email);
+
+        //then
+        assertThat(result).isFalse();
+    }
+}
