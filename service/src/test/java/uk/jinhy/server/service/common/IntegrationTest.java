fd2e5cc2cb62e39248db7efaa4319cc81f45a10b
jinhyeokfang <jinhyeokfang@gmail.com>
2025-03-04 11:14:37 +0000

    fix: Ryuk 리소스 리퍼 비활성화

--- service/src/test/java/uk/jinhy/server/service/common/IntegrationTest.java
+++ service/src/test/java/uk/jinhy/server/service/common/IntegrationTest.java
@@ -22,6 +22,9 @@ public abstract class IntegrationTest {
     protected static final GenericContainer<?> redisContainer;
 
     static {
+        System.setProperty("testcontainers.reuse.enable", "true");
+        System.setProperty("testcontainers.ryuk.disabled", "true");
+
         mysqlContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8"))
                 .withDatabaseName("test")
                 .withUsername("test")
