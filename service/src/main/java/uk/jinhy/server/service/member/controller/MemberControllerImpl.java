dfd7340fde142ff36e43f97b819fced67ce3a338
jinhyeokfang <jinhyeokfang@gmail.com>
2025-03-14 00:55:07 +0000

    chore: devcontainer 설정 변경

--- service/src/main/java/uk/jinhy/server/service/member/controller/MemberControllerImpl.java
+++ service/src/main/java/uk/jinhy/server/service/member/controller/MemberControllerImpl.java
@@ -2,6 +2,7 @@ package uk.jinhy.server.service.member.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import uk.jinhy.server.api.member.presentation.MemberDto;
 import uk.jinhy.server.service.member.service.MemberServiceImpl;
 import uk.jinhy.server.api.common.ApiResponse;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class MemberControllerImpl {
@@ -18,6 +20,7 @@ public class MemberControllerImpl {
 
     @PostMapping("/members")
     public ApiResponse<MemberDto.MemberSaveResponseDto> save(@Valid @RequestBody MemberDto.MemberSaveRequestDto request) {
+        log.info("HTTP 요청 발생");
         Member member = memberService.save(request.toEntity());
         return ApiResponse.success(MemberDto.MemberSaveResponseDto.of(member));
     }
