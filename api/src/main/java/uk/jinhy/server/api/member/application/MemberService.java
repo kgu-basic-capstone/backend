6d2b9c24620e408d08b9b213015306de0728216f
jinhyeokbang <jinhyeokfang@gmail.com>
2025-05-21 10:58:08 +0000

    refactor: 미사용 코드 제거 (KGU-25)

--- api/src/main/java/uk/jinhy/server/api/member/application/MemberService.java
+++
@@ -1,7 +0,0 @@
-package uk.jinhy.server.api.member.application;
-
-import uk.jinhy.server.api.member.domain.Member;
-
-public interface MemberService {
-    Member save(Member request);
-}
--- service/src/main/java/uk/jinhy/server/service/member/domain/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.jinhy.server.service.member.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    boolean existsByEmail(String email);
-}
